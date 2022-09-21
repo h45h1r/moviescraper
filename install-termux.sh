8766d16207f3979bc171a974f791321c3bce2b0e
h45h1r <87898213+h45h1r@users.noreply.github.com>
2022-09-21 23:58:12 +0300

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-pkg install -y update 
-pkg install -y python
 pip install beautifulsoup4
 pip install requests
-pkg install -y nodejs
-npm install -g peerflix
 curl -sL "https://raw.githubusercontent.com/h45h1r/moviescraper/master/moviescraper-termux" -o moviescraper
 chmod +x moviescraper
 mv moviescraper /data/data/com.termux/files/usr/bin/
